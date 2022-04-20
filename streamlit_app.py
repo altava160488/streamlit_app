0c9170dc8703f789747c046f6ceb74081fdcb79d
altava160488 <66040553+altava160488@users.noreply.github.com>
2022-04-20 17:41:20 +0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
 import streamlit
 streamlit.title('My new healthy dinner')
-print('the same')
+
+streamlit.header('Breackfast manu')
+streamlit.text('Omega 3 & Blueberry Outmeal')
+streamlit.text('Kale, Spinach and Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
