b1326a78a750ac2ab82fa72dab147ff27f6b3310
altava160488 <66040553+altava160488@users.noreply.github.com>
2022-04-21 11:40:30 +0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,11 @@
 import streamlit
+import pandas as pd
 streamlit.title('My new healthy dinner')
 
 streamlit.header('Breakfast Favourites')
 streamlit.text('🥑 Omega 3 & Blueberry Outmeal🍇')
 streamlit.text('🥗Kale, Spinach and Rocket Smoothie🥛')
 streamlit.text('🥚Hard-Boiled Free-Range Egg🥚')
+
+my_list_of_csv = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_list_of_csv
