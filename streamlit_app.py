73d454f3dcb704e791ceb7c8a2c90b3e10c306ed
altava160488 <66040553+altava160488@users.noreply.github.com>
2022-04-21 12:04:07 +0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,7 +13,12 @@ streamlit.text('🥚Hard-Boiled Free-Range Egg🥚')
 
 streamlit.header('🍉🍒Build your own fruit smoothie🍓🍊')
 my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-#my_fruit_list
-
+"""
 streamlit.multiselect("Pick some fruits:", list(my_fruit_list["Fruit"]))
+my_fruit_list
+
+"""
+
+my_fruit_list = my_fruit_list.set_index('Fruit')
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
 streamlit.dataframe(my_fruit_list)
