15dfab5b722786ca1eff44ba46318f594d8b43d3
altava160488 <66040553+altava160488@users.noreply.github.com>
2022-04-21 11:48:22 +0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,5 +11,5 @@ streamlit.text('🥚Hard-Boiled Free-Range Egg🥚')
 # my_list_of_scv 
 #streamlit.dataframe(my_list_of_scv)
 
-my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 streamlit.dataframe(my_fruit_list)
