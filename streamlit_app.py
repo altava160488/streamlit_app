0cd041a13f0ebd5339a94f7057fe79038324d8f3
altava160488 <66040553+altava160488@users.noreply.github.com>
2022-04-21 11:42:34 +0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,4 +8,5 @@ streamlit.text('🥗Kale, Spinach and Rocket Smoothie🥛')
 streamlit.text('🥚Hard-Boiled Free-Range Egg🥚')
 
 my_list_of_csv = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-my_list_of_csv
+""" my_list_of_scv """
+streamlit.dataframe(my_fruit_list)
